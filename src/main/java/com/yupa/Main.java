ed95f0895b49e1efd72f1fd8239af107c761b799
cyupa <cesar_yupa@hotmail.com>
2022-07-24 17:15:04 -0500

    se programa patron factory

--- src/main/java/com/yupa/Main.java
+++ src/main/java/com/yupa/Main.java
@@ -1,11 +1,14 @@
 package com.yupa;
 
+import com.yupa.factory.FactoryPago;
+import com.yupa.factory.TypePago;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hola a todos");
-		
+		var obj = FactoryPago.builderPago(TypePago.EFECTIVO);
+		obj.pago();
 		
 	}
 
